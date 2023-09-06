0c24ff574def93bdea413a380a2d9c525021dc13
Mohamed Ahmed <ma07041705@gmail.com>
2023-09-06 08:32:38 +0300

    fixed error

--- Server/Controllers/Logistic/medicineCategoryController.js
+++ Server/Controllers/Logistic/medicineCategoryController.js
@@ -1,6 +1,21 @@
 const MedicineCategory = require('../../Models/medicineCategoryModel')
 
 
+const createMedicineCategory = async (req, res) => {
+    const {medicine_category } = req.body;
+
+    if(!medicine_category){
+      return res.status(400).json({ success: false, message: "All Fields Are Required"});
+    }
+    try {
+      const medicineCategory = await MedicineCategory.create({
+            medicine_category: medicine_category,
+        });
+        return res.status(200).json({ success: true, medicineCategory});
+    } catch (error) {
+        res.json({ message: error.message });
+    }  
+}
 
 
 const getAllMedicineCategory = async (req, res) => {
@@ -14,9 +29,48 @@ const getAllMedicineCategory = async (req, res) => {
 
 
  
+const getMedicineCategoryById = async (req, res) => {
+  const id = req.params.id
+  try {
+    const medicineCategoryById = await MedicineCategory.findAll({ where: { medicine_category_id: id} });
+    return res.status(200).json({success: true, medicineCategory: medicineCategoryById, });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+
+
+const UpdateMedicineCategory = async (req, res) => {
+  const id = req.params.id
+    const {medicine_category } = req.body;
+    try {  
+      const UpdateMedicineCategory = await MedicineCategory.update({medicine_category: medicine_category },
+        {where: {medicine_category_id: id} });
+      return res.status(200).json({ success: true, medicineCategory: UpdateMedicineCategory});
+    } catch (error) {
+      return res.status(500).json({ success: false, message: error.message});
+    }
+};
+
+ 
+
+const deleteMedicineCategory = async (req, res) => {
+  try {
+      await MedicineCategory.destroy({where: {medicine_category_id: req.params.id}});
+
+    return res.status(200).json({ success: true, medicineCategory: deleteMedicine});
+  } catch (error) {
+      res.json({ message: error.message });
+  }  
+}
 
  
 module.exports = {
+    createMedicineCategory,
     getAllMedicineCategory,
+    getMedicineCategoryById,
+    UpdateMedicineCategory,
+    deleteMedicineCategory
 };
   
