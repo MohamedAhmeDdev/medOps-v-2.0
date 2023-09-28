e659aa497683230870ef1fc3603a406d53152f1b
Mohamed Ahmed <ma07041705@gmail.com>
2023-09-28 21:56:24 +0300

    notification context

--- app/src/utils/context/NotificationContext.js
+++ app/src/utils/context/NotificationContext.js
@@ -1,57 +1,91 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const NotificationContext = createContext();
 
-const initialState = {
-  count: 0,
-  notifications: [],
-};
-
-const notificationReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_NOTIFICATION':
-      const isNewNotification = !state.notifications.some(
-        (notification) => notification.id === action.payload.id
-      );
-
-      if (isNewNotification) {
-        return {
-          ...state,
-          count: state.count + 1,
-          notifications: [...state.notifications, action.payload],
-        };
-      }
-      return state;
-
-    case 'MARK_NOTIFICATION_AS_READ':
-      const notificationId = action.payload.id;
-      return {
-        ...state,
-        count: state.count > 0 ? state.count - 1 : 0,
-        notifications: state.notifications.map((notification) =>
-          notification.id === notificationId
-            ? { ...notification, read: true }
-            : notification
-        ),
-      };
-
-    default:
-      return state;
-  }
-};
-
-const NotificationProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(notificationReducer, initialState);
-
-  return (
-    <NotificationContext.Provider value={{ state, dispatch }}>
-      {children}
-    </NotificationContext.Provider>
-  );
-};
-
-const useNotification = () => {
+export const useNotification = () => {
   return useContext(NotificationContext);
 };
 
-export { NotificationProvider, useNotification };
+export const NotificationProvider = ({ children }) => {
+  const [successNotification, setSuccessNotification] = useState(null);
+  const [errorNotification, setErrorNotification] = useState(null);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  const showSuccessNotification = (message) => {
+    setSuccessNotification(message); // Use successNotification
+    setShowSuccess(true);
+    setTimeout(() => hideSuccessNotification(), 5000); // Auto-hide after 5 seconds
+  };
+
+  const showErrorNotification = (message) => {
+    setErrorNotification(message); // Use errorNotification
+    setShowError(true);
+    setTimeout(() => hideErrorNotification(), 5000); // Auto-hide after 5 seconds
+  };
+
+  const hideSuccessNotification = () => {
+    setShowSuccess(false);
+    setTimeout(() => setSuccessNotification(null), 300); // Delay to allow slide-out animation
+  };
+
+  const hideErrorNotification = () => {
+    setShowError(false);
+    setTimeout(() => setErrorNotification(null), 200); // Delay to allow slide-out animation
+  };
+
+  const removeSuccessNotification = () => {
+    hideSuccessNotification();
+  };
+
+  const removeErrorNotification = () => {
+    hideErrorNotification();
+  };
+
+  useEffect(() => {
+    if (showSuccess || showError) {
+      const timeout = setTimeout(() => {
+        hideSuccessNotification();
+        hideErrorNotification();
+      }, 5000); // Auto-hide after 5 seconds
+      return () => clearTimeout(timeout);
+    }
+  }, [showSuccess, showError]);
+
+
+  return (
+    <NotificationContext.Provider
+      value={{
+        showSuccessNotification,
+        showErrorNotification,
+        removeSuccessNotification,
+        removeErrorNotification,
+      }}
+    >
+      {children}
+      <div className={`${ showSuccess ? '-translate-x-0 opacity-100' : '-translate-x-full opacity-0'
+        } flex justify-around transform fixed w-72 h-20 bottom-10 left-10 right-0 bg-green-400 text-white p-2 rounded-lg transition-transform duration-300 ease-in-out`}
+      >
+       <div className='bg-white rounded-full w-8 h-8 mt-5'>
+         <span class="material-symbols-outlined mx-auto pt-1 pl-1 text-green-300">check</span>
+       </div>
+        <div className='mt-5'>{successNotification}</div>
+        <button onClick={removeErrorNotification} className="ml-2 text-white font-bold absolute top-2 right-1">
+          <span class="material-symbols-outlined">close</span>
+        </button>
+      </div>
+
+      <div className={`${ showError ? '-translate-x-0 opacity-100' : '-translate-x-full opacity-0'
+        } flex justify-around transform fixed w-72 h-20 bottom-10 left-10 right-0 bg-red-400 text-white p-2 rounded-lg transition-transform duration-300 ease-in-out`}
+      >
+          {/* <div className='bg-white rounded-full w-8 h-8 mt-5'> */}
+           <span class="material-symbols-outlined mx-auto mt-5 pt-1 pl-1 text-white">error</span>
+         {/* </div> */}
+          <div className='mt-5'>{errorNotification}</div>
+        <button onClick={removeErrorNotification} className="ml-2 text-white font-bold absolute top-2 right-1">
+          <span class="material-symbols-outlined">close</span>
+        </button>
+      </div>
+    </NotificationContext.Provider>
+  );
+};
