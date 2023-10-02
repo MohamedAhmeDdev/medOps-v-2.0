f5fc010f833e5a6b7ec9af9ab5bd0806db1c8232
Mohamed Ahmed <ma07041705@gmail.com>
2023-10-02 16:59:12 +0300

    added logout function

--- app/src/Component/LogisticSidebar.js
+++ app/src/Component/LogisticSidebar.js
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../assets/img/logo.jpg'
+import { UseAuthContext } from "../utils/Hook/StaffAuth";
 
 
 function LogisticSidebar({ sidebarOpen, toggleSidebar }) {
   const [openCategory, setOpenCategory] = useState(false);
   const [openMedicine, setOpenMedicine] = useState(false);
+  const { dispatch } = UseAuthContext();
+  const navigate = useNavigate();
+
+  const logoutUser = () => {
+    localStorage.removeItem("user");
+    dispatch({ type: "LOGOUT" });
+
+    navigate("/");
+  };
+
 
   return (
     <aside  className={`flex flex-1 bg-gray-100 w-60 ${sidebarOpen ? 'block fixed md:static z-20' : 'hidden md:block'}`} style={{ height: '100vh', overflowY: 'auto' }}>
@@ -121,7 +132,7 @@ function LogisticSidebar({ sidebarOpen, toggleSidebar }) {
         </li>
       </ul>
       <ul className="bg-gray-100 flex flex-col pl-0 mb-0 pt-5 space-y-2.5 pb-10 absolute w-60 bottom-10">
-          <li className="mt-0.5 w-full">
+      <li  onClick={logoutUser} className="mt-0.5 w-full cursor-pointer">
             <div className="py-2.5 px-10 text-sm hover:shadow-lg bg-white ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg  font-semibold text-slate-700 transition-colors">
               <div className="shadow-lg mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center  p-5">
               <span className="material-symbols-outlined">logout</span>
