b0851c006b67287aa47b6f598fd0b10287390c50
Mohamed Ahmed <ma07041705@gmail.com>
2024-09-02 18:05:14 +0300

    package installation

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,9 +2,13 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+     "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
 }
