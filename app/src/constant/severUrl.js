5392439854132f09c73487fa564a12ddf297c7ff
Mohamed Ahmed <ma07041705@gmail.com>
2024-09-07 17:39:27 +0300

    added several sever url for each staff

--- app/src/constant/severUrl.js
+++ app/src/constant/severUrl.js
@@ -1,1 +1,5 @@
 export const SERVER_URL = "http://localhost:5000";
+export const MANAGER_SERVER_URL = "http://localhost:5000";
+export const OPERATOR_SERVER_URL = "http://localhost:5000";
+export const TRANSPORTER_SERVER_URL = "http://localhost:5000";
+export const LOGISTICS_SERVER_URL = "http://localhost:5000";
