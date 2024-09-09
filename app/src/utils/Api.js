8b0f4b4962a88a7f8393bfb34777bb4133bbcbb8
Mohamed Ahmed <ma07041705@gmail.com>
2024-09-09 14:37:25 +0300

    remove the base server url to be universal to all users

--- app/src/utils/Api.js
+++ app/src/utils/Api.js
@@ -1,24 +1,31 @@
-import { SERVER_URL } from "../constant/severUrl";
 import axios from "axios";
-
 
 export const Api = async (url, method, data = {}, headers = {}) => {
 
   const user = localStorage.getItem("user");
-  const token = user ? JSON.parse(user).token : null; 
-  const config = { headers: { Authorization: `${token}`, ...headers }};
+  const token = user ? JSON.parse(user): null; 
+  if (!token) {
+    console.error("Token is missing or invalid in localStorage");
+  }
+
+  const config = {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '', 
+      ...headers,
+    },
+  };
+
   try {
     const res = await axios({
       method,
       url,
       data,
-      baseURL: SERVER_URL,
       ...config,
     });
     return res.data;
   } catch (err) {
     const errMessage = err.response?.data?.message || "Something went wrong";
-    console.log(errMessage);
+    console.log("API Error:", errMessage);
     throw err;
   }
 };
