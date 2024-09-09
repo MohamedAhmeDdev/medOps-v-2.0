2d5780b676957e464972b5f8d1f1f391b30fc631
Mohamed Ahmed <ma07041705@gmail.com>
2024-09-09 14:42:50 +0300

    manager routes

--- app/src/Dashboards/Manager/routes/index.js
+++ app/src/Dashboards/Manager/routes/index.js
@@ -2,17 +2,83 @@
 
 import { lazy } from 'react'
 
-// const Dashboard = lazy(() => import('../pages/Login'))
+const Dashboard = lazy(() => import('../Pages/Dashboard'))
+const MedicineCategory = lazy(() => import('../Pages/MedicineCategory'))
+const Medicine = lazy(() => import('../Pages/Medicine'))
+const Users = lazy(() => import('../Pages/User'))
+const PasswordRequest = lazy(() => import('../Pages/PasswordRequest'))
+const AddStaff = lazy(() => import('../Pages/Staff/CreateStaff'))
+const StaffList = lazy(() => import('../Pages/Staff/Staff'))
+const ShiftList = lazy(() => import('../Pages/Staff/SingleStaffShift'))
+const UpdateStaff = lazy(() => import('../Pages/Staff/UpdateStaffInfo'))
+const AddTransport = lazy(() => import('../Pages/Transport/CreateTransport'))
+const TransportList = lazy(() => import('../Pages/Transport/Transport'))
+const UpdateTransport = lazy(() => import('../Pages/Transport/UpdateTransport'))
+const AddSupplier = lazy(() => import('../Pages/Supplier/CreateSupplier'))
+const SupplierList = lazy(() => import('../Pages/Supplier/Supplier'))
+const UpdateSupplier = lazy(() => import('../Pages/Supplier/UpdateSupplier'))
+
 
 
 
 
 const routes = [
   {
-    path: '/dashboard', // the url
-    // component: Dashboard, // view rendered
+    path: '/medicine-category',
+    component: MedicineCategory,
   },
-
+  {
+    path: '/medicine-list',
+    component: Medicine,
+  },
+  {
+    path: '/users',
+    component: Users,
+  },
+  {
+    path: '/password-request',
+    component: PasswordRequest,
+  },
+  {
+    path: '/add-staff',
+    component: AddStaff,
+  },
+  {
+    path: '/staff-lists',
+    component: StaffList,
+  },
+  {
+    path: '/shift/:staff_id',
+    component: ShiftList,
+  },
+    {
+    path: '/:id',
+    component: UpdateStaff,
+  },
+  {
+    path: '/add-transport',
+    component: AddTransport,
+  },
+  {
+    path: '/transport-lists',
+    component: TransportList,
+  },
+  {
+    path: '/transport/:id',
+    component: UpdateTransport,
+  },
+  {
+    path: '/add-supplier',
+    component: AddSupplier,
+  },
+  {
+    path: '/supplier-lists',
+    component: SupplierList,
+  },
+  {
+    path: '/supplier/:id',
+    component: UpdateSupplier
+  },
 ]
 
 export default routes
--- app/src/Dashboards/Manager/routes/sidebar.js
+++ app/src/Dashboards/Manager/routes/sidebar.js
@@ -8,41 +8,63 @@ const submenuIconClasses = `h-5 w-5`
 const routes = [
 
   {
-    path: '/app/dashboard',
-    name: 'Dashboard',
+    path: '/manager/medicine-category',
+    name: 'Medicine Category',
   },
   {
-    path: '/app/leads', // url
-    name: 'Leads', // name that appear in Sidebar
+    path: '/manager/medicine-list',
+    name: 'Medicine',
   },
   {
-    path: '/app/transactions', // url
-  
-    name: 'Transactions', // name that appear in Sidebar
+    path: '/manager/users',
+    name: 'Users',
   },
   {
-    path: '', //no url needed as this has submenu
- 
-    name: 'Settings', // name that appear in Sidebar
+    path: '/manager/password-request',
+    name: 'Password Request',
+  },
+  {
+    path: '', 
+    name: 'Staffs',
     submenu : [
       {
-        path: '/app/settings-profile', //url
- 
-        name: 'Profile', // name that appear in Sidebar
+        path: '/manager/add-staff', 
+        name: 'Add Staff',
       },
       {
-        path: '/app/settings-billing',
-       
-        name: 'Billing',
-      },
-      {
-        path: '/app/settings-team', // url
-     
-        name: 'Team Members', // name that appear in Sidebar
+        path: '/manager/staff-lists',
+        name: 'Staff List',
       },
     ]
   },
-  
+  {
+    path: '', 
+    name: 'Transports',
+    submenu : [
+      {
+        path: '/manager/add-transport', 
+        name: 'Add Transport',
+      },
+      {
+        path: '/manager/transport-lists',
+        name: 'Transport List',
+      },
+    ]
+  },
+  {
+    path: '', 
+    name: 'Supplier',
+    submenu : [
+      {
+        path: '/manager/add-supplier', 
+        name: 'Add Supplier',
+      },
+      {
+        path: '/manager/supplier-lists',
+        name: 'Supplier List',
+      },
+    ]
+  },
 ]
 
 export default routes
