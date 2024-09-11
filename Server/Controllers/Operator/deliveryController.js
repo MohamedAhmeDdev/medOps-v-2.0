b8a0a43401d7bafe16053cd8467565fa749a3bcd
Mohamed Ahmed <ma07041705@gmail.com>
2024-09-11 17:01:07 +0300

    added API for the assigning order to deliveries

--- Server/Controllers/Operator/deliveryController.js
+++ Server/Controllers/Operator/deliveryController.js
@@ -8,6 +8,46 @@ const Medicine = require('../../Models/medicine')
 
 
 
+const assignOrders = async (req, res) => {
+  const { staff_id, orders } = req.body;
+
+  try {
+    const transport = await Transport.findOne({ where: { staff_id } });
+
+    if (!transport) {
+      return res.status(404).json({ success: false, message: 'Transport not found' });
+    }
+
+
+    const deliveries = orders.map(order => ({
+      order_id: order,
+      transport_id: transport.transport_id,
+      delivery_status: 'Pending',
+      delivery_date: new Date(),
+    }));
+
+ 
+    const createdDeliveries = await Delivery.bulkCreate(deliveries);
+
+   
+    await Promise.all(orders.map(orderId =>
+      Order.update({ order_status: 'Assigned' }, { where: { order_id: orderId } })
+    ));
+
+    return res.status(200).json({
+      success: true,
+      message: 'Orders assigned',
+      deliveries: createdDeliveries,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+
+
 
 
 const getAllDelivery = async (req, res) => {
@@ -27,14 +67,16 @@ const getAllDelivery = async (req, res) => {
           attributes: ['name', 'phoneNumber'],
         }]
       }],
-      order: [[ Order ,'order_date','DESC']]
+      order: [[ Order ,'order_date','ASC']]
     });
 
     if(!AllDelivery){
       return res.status(400).json({success: true,  message: "delivery not found" });
     }
 
-    return res.status(200).json({ success: true, delivery: AllDelivery});
+    const filteredDelivery = AllDelivery.filter(status => status.delivery_status === 'Pending' );  
+
+    return res.status(200).json({ success: true, delivery: filteredDelivery});
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
@@ -76,6 +118,7 @@ const getDeliveryById = async (req, res) => {
 
 
 module.exports = {
+  assignOrders,
   getAllDelivery,
   getDeliveryById
 };
