522245c3a97322203a8c9601668d2ddc7da13f57
Mohamed Ahmed <ma07041705@gmail.com>
2024-09-11 22:09:31 +0300

    corrected the sever url

--- app/src/constant/severUrl.js
+++ app/src/constant/severUrl.js
@@ -1,5 +1,5 @@
 export const SERVER_URL = "http://localhost:5000";
 export const MANAGER_SERVER_URL = "http://localhost:5000/manager";
 export const OPERATOR_SERVER_URL = "http://localhost:5000/operator";
-export const TRANSPORTER_SERVER_URL = "http://localhost:5000/logistic";
-export const LOGISTICS_SERVER_URL = "http://localhost:5000/transport";
+export const LOGISTICS_SERVER_URL  = "http://localhost:5000/logistic";
+export const TRANSPORTER_SERVER_URL = "http://localhost:5000/transport";
