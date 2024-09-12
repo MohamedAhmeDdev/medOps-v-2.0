3c84a778a51a147f8740316d2e2909869ab68f97
Mohamed Ahmed <ma07041705@gmail.com>
2024-09-12 17:48:17 +0300

    staff APIs implementation of shifts

--- Server/Controllers/Staff/shiftLogsController.js
+++ Server/Controllers/Staff/shiftLogsController.js
@@ -1,5 +1,5 @@
 const Shift = require('../../Models/shiftLogs')
-const User = require('../../Models/user')
+const ShiftLogs = require('../../Models/shiftLogs')
 const Staff = require('../../Models/staff')
 const Transport = require('../../Models/transport')
 const { Op } = require('sequelize');
@@ -8,34 +8,40 @@ const { Op } = require('sequelize');
 
 
 const StartShift = async (req, res) => {
-  // const user_id = req.user.id;
-    const {staff_id } = req.body;
+  const staff_id = req.user.staff_id; 
     if(!staff_id){
-      return res.status(400).json({success: false, message: "All Fields Are Required"});
+      return res.status(400).json({success: false, message: "staff is Required"});
     }
+    
     try {
       const findStaff = await Staff.findOne({where:{ staff_id: staff_id}})
       if(!findStaff){
         return res.status(400).json({success: false, message: "staff not found"});
       }
-
+      
       const shift = await Shift.create({
         staff_id: staff_id,
-        shift_status: "Logged In", 
+        shift_status: "Clocked In", 
         start_time: new Date(),
         Date: new Date(),
       });
 
-      return res.status(200).json({ success: true, shift });
-    } catch (error) {
+      return res.status(200).json({
+         success: true,
+         message: "Clock In",
+         shift
+      });
+    } catch (error) {      
       return res.status(500).json({ success: false, message: error.message });
     }  
 }
 
 
 
+
 const EnterEndTime = async (req, res) => {
-  const id = req.params.id;
+  const staff_id = req.user.staff_id; 
+console.log(staff_id);
 
   const startOfDay = new Date();
   startOfDay.setUTCHours(0, 0, 0, 0);
@@ -45,7 +51,7 @@ const EnterEndTime = async (req, res) => {
   try {
     const staff = await Shift.findOne({
       where: {
-        staff_id: id,
+        staff_id: staff_id,
         Date: {
           [Op.between]: [startOfDay.toISOString(), endOfDay.toISOString()]
         }
@@ -57,21 +63,24 @@ const EnterEndTime = async (req, res) => {
     }
 
     const [updated] = await Shift.update({
-        shift_status: "Logged Out",
+        shift_status: "Clocked Out",
         end_time: new Date(),
       },{
         where: {
-          staff_id: id,
+          staff_id: staff.staff_id,
           Date: staff.Date
         }}
     );
 
-    if (updated) {
-      return res.status(200).json({ success: true, updated });
-    } else {
-      return res.status(400).json({ success: false, message: "Failed to update shift" });
-    }
+    return res.status(200).json({ 
+      success: true,
+      message: "Clock Out",
+      updated
+    });
+   
   } catch (error) {
+    console.log(error);
+    
     return res.status(500).json({ success: false, message: error.message });
   }
 };
@@ -79,14 +88,24 @@ const EnterEndTime = async (req, res) => {
 
 
 
-
-
 const getShifts = async (req, res) => {
-  const id = req.params.id;
+  const staff_id = req.user.staff_id; 
+  const { weekOffset = 0 } = req.query;
 
   try {
+    let startOfWeek = new Date();
+    startOfWeek.setDate(startOfWeek.getDate() - startOfWeek.getDay() - (weekOffset * 7));
+    let endOfWeek = new Date(startOfWeek);
+    endOfWeek.setDate(startOfWeek.getDate() + 6); 
+
     const shift = await Shift.findAll( {
-      where: { staff_id: id }
+      where: {
+         staff_id: staff_id,
+         Date: {
+          [Op.between]: [startOfWeek, endOfWeek],
+        },
+      },
+      order: [['Date', 'ASC']],
     });
 
     return res.status(200).json({ success: true, shift });
@@ -97,8 +116,47 @@ const getShifts = async (req, res) => {
 
 
 
+const getCurrentShifts = async (req, res) => {
+  const staff_id = req.user.staff_id; 
+  
+  try {
+    const startOfDay = new Date();
+    startOfDay.setUTCHours(0, 0, 0, 0);
+    const endOfDay = new Date();
+    endOfDay.setUTCHours(23, 59, 59, 999);
+
+    const shift = await Staff.findOne({
+      where:{ staff_id: staff_id},
+      include: [{
+        model: ShiftLogs,
+        attributes: ['shift_status', 'start_time', 'end_time', 'Date'],
+        where: {
+          Date: {
+            [Op.between]: [startOfDay.toISOString(), endOfDay.toISOString()]
+          }
+        },
+        required: false,
+      }],
+    });
+
+    if (!shift || shift.length === 0) {
+      return res.status(400).json({ success: false, message: "No staff or shifts found for the current day" });
+    }
+
+    return res.status(200).json({ success: true, shift });
+  } catch (error) {
+    console.log(error);
+    
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+
+
+
 module.exports = {
     StartShift,
+    getCurrentShifts,
     EnterEndTime,
     getShifts
 };
--- Server/Routes/Staff/ShiftRoute.js
+++ Server/Routes/Staff/ShiftRoute.js
@@ -1,15 +1,17 @@
 const {
     StartShift,
+    getCurrentShifts,
     EnterEndTime,
     getShifts
 } = require("../../Controllers/Staff/shiftLogsController");
 
 const AuthRouter = require("express").Router();
-  
+const { verifyToken } = require("../../middleware/VerifyToken");
 
-AuthRouter.get("/:id", getShifts);
-AuthRouter.post("/", StartShift);
-AuthRouter.patch("/:id", EnterEndTime);
+AuthRouter.get("/", verifyToken, getShifts);
+AuthRouter.get("/currentShift", verifyToken, getCurrentShifts);
+AuthRouter.post("/",verifyToken , StartShift);
+AuthRouter.post("/clockOut", verifyToken, EnterEndTime);
 
   
   
