152a8eb90299ce0881dd003fd8a2f5ea034614d0
Mohamed Ahmed <ma07041705@gmail.com>
2024-09-15 14:27:27 +0300

    i have separated the upload image and upload document

--- Server/middleware/multer.js
+++ Server/middleware/multer.js
@@ -1,15 +1,34 @@
 const multer = require("multer");
 const path = require("path");
 
-const maxSize = 5 * 1024 * 1024; // 5MB
+const maxSize = 5 * 1024 * 1024; 
+const storage = multer.memoryStorage(); 
 
-const storage = multer.memoryStorage(); // Store files in memory to upload to Cloudinary
 
 const uploadImage = multer({
-  storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+  storage: multer.diskStorage({}),
+  limits: { fileSize: maxSize },
   fileFilter: (req, file, cb) => {
-    const fileTypes = /jpeg|jpg|png|gif|pdf|webp/;
+    const fileTypes = /jpeg|jpg|png|gif|webp/;
+    const extName = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimeType = fileTypes.test(file.mimetype);
+
+    if (extName && mimeType) {
+      return cb(null, true);
+    } else {
+      cb("Images only");
+    }
+  },
+}).single("image");
+
+
+const uploadDocument = multer({
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 },
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /pdf/;
     const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
     const mimeType = fileTypes.test(file.mimetype);
 
@@ -23,4 +42,5 @@ const uploadImage = multer({
 
 module.exports = {
   uploadImage,
+  uploadDocument
 };
